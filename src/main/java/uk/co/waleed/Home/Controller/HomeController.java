eabec9777d5059eabb956c2b7fe1af22a8ed78c3
Waleed Ur Rehman <mac@waleeds-mbp.home>
2020-06-13 19:30:33 +0100

    Added endpoints with example to use on home endpoint

--- src/main/java/uk/co/waleed/Home/Controller/HomeController.java
+++ src/main/java/uk/co/waleed/Home/Controller/HomeController.java
@@ -14,8 +14,12 @@ public class HomeController{
     @RequestMapping("/")
     @ResponseBody
     public String handleError() {
-        return String.format("<center><html><body><h1>Welcome to Java Web Service</h1><div>"
-                        + "<b>Please use one of the end points to use the webservice such as 'permuation' or 'greeting'"
+        return String.format("<center><h1>Welcome to Java Web Service</h1>"
+                        + "<div><b>Please use one of the end points to use the webservice"
+                        + "<br />'permutation' requires parameter permuteInt: "
+                        + "<a href='/permutation?permuteInt=123'>/permutation?permuteInt=123</a>"
+                        + "<br />'greeting' requires parameter name: "
+                        + "<a href='/greeting?name=Waleed'>/greeting?name=Waleed</a>"
                         + "</b></div></center>");
     }
 }
