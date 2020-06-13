39da2fbede3facd8d0b4e8772c18d63e6480cfcd
Waleed Ur Rehman <mac@waleeds-mbp.home>
2020-06-13 19:58:16 +0100

    Code Cleanup

--- src/main/java/uk/co/waleed/Error/Controller/ErrorHandlerController.java
+++ src/main/java/uk/co/waleed/Error/Controller/ErrorHandlerController.java
@@ -1,6 +1,5 @@
 package uk.co.waleed.Error.Controller;
 
-
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -16,8 +15,8 @@ public class ErrorHandlerController implements ErrorController {
     public String handleError(HttpServletRequest request) {
         Integer statusCode = (Integer) request.getAttribute("javax.servlet.error.status_code");
         Exception exception = (Exception) request.getAttribute("javax.servlet.error.exception");
-        return String.format("<center><html><body><h2>Error Page</h2><div>Status code: <b>%s</b></div>"
-                        + "<div>Exception Message: <b>%s</b></div><body></html></center>",
+        return String.format("<center><h2>Error Page</h2><div>Status code: <b>%s</b></div>"
+                        + "<div>Exception Message: <b>%s</b></div></center>",
                 statusCode, exception==null? "N/A": exception.getMessage());
     }
 
--- src/main/java/uk/co/waleed/Home/Controller/HomeController.java
+++ src/main/java/uk/co/waleed/Home/Controller/HomeController.java
@@ -1,12 +1,8 @@
 package uk.co.waleed.Home.Controller;
 
-
-import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.servlet.http.HttpServletRequest;
 
 @RestController
 public class HomeController{
@@ -15,11 +11,11 @@ public class HomeController{
     @ResponseBody
     public String handleError() {
         return String.format("<center><h1>Welcome to Java Web Service</h1>"
-                        + "<div><b>Please use one of the end points to use the webservice"
+                        + "<b>Please use one of the end points to use the webservice"
                         + "<br />'permutation' requires parameter permuteInt: "
                         + "<a href='permutation?permuteInt=123'>/permutation?permuteInt=123</a>"
                         + "<br />'greeting' requires parameter name: "
                         + "<a href='greeting?name=Waleed'>/greeting?name=Waleed</a>"
-                        + "</b></div></center>");
+                        + "</b></center>");
     }
 }
